a2848c8fdf71654804f3005a4ecc74de431271a5
meloman1001 <meloman1001@mail.ru>
2020-11-13 19:57:30 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <div class="wrapper">
+        <header>
+           <div class="header__logo"></div>
+           <div class="header__phone">8 800 55-00-001</div>
+        </header>
+     </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import 'node_modules/bootstrap/scss/bootstrap.scss';
+@import 'node_modules/bootstrap-vue/src/index.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+.wrapper {
+   max-width: 1070px;
+   margin: 0 auto;
+   border: 1px solid red;
+}
+
+header {
+   height: 70px;
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+
+   .header__logo {
+      max-height: 32px;
+      max-width: 181px;
+      width: 100%;
+      height: 100%;
+      background: url('./assets/svg/mf_logo_full.svg') center / contain no-repeat;
+   }
+
+   .header__phone {
+
+   }
+}
+
 </style>
